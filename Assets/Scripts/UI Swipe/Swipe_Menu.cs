69c14dcbca1c278046da7f72241b07d7280502a6
Mordechai <demarpacman@gmail.com>
2024-04-14 06:33:54 +0700

    UI Swipe Test

--- Assets/Scripts/UI Swipe/Swipe_Menu.cs
+++ Assets/Scripts/UI Swipe/Swipe_Menu.cs
@@ -40,15 +40,15 @@ public class Swipe_Menu : MonoBehaviour
         for (int i = 0; i < pos.Length; i++) 
         {
             if (scroll_pos < pos[i] + (distance / 2 ) && scroll_pos > pos[i] - (distance / 2 )) 
-        }
-
-        {
-            transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.1f);
-        
-            for (int a = 0; a < pos.Length; a++)
-        
-            if (a != i){
-                transform.GetChild(a).localScale = Vector2.Lerp(transform.GetChild(a).localScale, new Vector2(0.8f, 0.8f), 0.1f);
+            {
+                transform.GetChild (i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.1f);
+                for (int a = 0; a < pos.Length; a++)
+                {
+                    if (a != i)
+                    {
+                        transform.GetChild(a).localScale = Vector2.Lerp(transform.GetChild(a).localScale, new Vector2(0.8f, 0.8f), 0.1f);
+                    }
+                }
             }
         }
     }
